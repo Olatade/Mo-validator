43dbf306ca632fe963ec6e1205014470d707cfd5
Olatade Abiona <4tade01@gmail.com>
2021-01-01 22:22:35 +0100

    Add form style

--- mo_validator.js
+++ mo_validator.js
@@ -1,1 +1,10 @@
 console.log('js is working');
+
+
+const errorMessage = document.querySelector(".form-group[data-group-id='middleName']");
+console.log(errorMessage);
+
+
+setTimeout( function(){
+    errorMessage.classList.add('error');
+}, 2000);
